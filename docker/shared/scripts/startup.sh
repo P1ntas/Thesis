0e2d3c5ad94d0fb4e178c7b36d4e605f5aaf7949
P1ntas <up202008014@g.uporto.pt>
2025-05-23 11:38:55 +0100

    Added analyze

--- docker/shared/scripts/startup.sh
+++ docker/shared/scripts/startup.sh
@@ -20,14 +20,17 @@ mkdir $RESULTS_ROARING_DIR
 mkdir "$RESULTS_ROARING_DIR"/duckdb
 mkdir "$RESULTS_ROARING_DIR"/datafusion
 mkdir "$RESULTS_ROARING_DIR"/plots
+mkdir "$RESULTS_ROARING_DIR"/analyze
 mkdir $RESULTS_FAST_DIR
 mkdir "$RESULTS_FAST_DIR"/duckdb
 mkdir "$RESULTS_FAST_DIR"/datafusion
 mkdir "$RESULTS_FAST_DIR"/plots
+mkdir "$RESULTS_FAST_DIR"/analyze
 mkdir $RESULTS_KDTREE_DIR
 mkdir "$RESULTS_KDTREE_DIR"/duckdb
 mkdir "$RESULTS_KDTREE_DIR"/datafusion
 mkdir "$RESULTS_KDTREE_DIR"/plots
+mkdir "$RESULTS_KDTREE_DIR"/analyze
 mkdir $PLOTS_DIR
 mkdir "$PLOTS_DIR"/tpch
 mkdir "$PLOTS_DIR"/tpcds
